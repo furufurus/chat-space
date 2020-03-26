5f421f0d6e70143c2ee702608ee2ae1a3dc1f69b
furufurus <s.furu444@ozzio.jp>
2020-03-26 23:34:15 +0900

    finish_devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,8 @@
-# Rails.application.routes.draw do
-  devise_for :users
-#   # get 'messages/index'
-#   root "groups#index"
-#   # root "messages#index"
-# end
 Rails.application.routes.draw do
-
   devise_for :users
-  # devise_for :users
-  root "messages#index"
-  # resources :users, only: [:index, :edit, :update]
-  # resources :groups, only: [:index, :new, :create, :edit, :update] do
-    # resources :messages, only: [:index, :create]
-    # namespace :api do
-    #   resources :messages, only: :index, defaults: { format: 'json'}
-    # end
-  end
+  # 下の行は削除する
+  # get 'messages/index
 
-# end
+  root "messages#index"
+  resources :users, only: [:edit, :update]
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200312124108) do
-
-  create_table "scores", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name"
-    t.integer  "score"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_scores_on_user_id", using: :btree
-  end
+ActiveRecord::Schema.define(version: 20200326131807) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",       null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "email"
+    t.string   "name",                                null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["name"], name: "index_users_on_name", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  add_foreign_key "scores", "users"
 end
