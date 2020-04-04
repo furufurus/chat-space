70bb21ebf4e42963a9f30944643665c9e4742efa
furufurus <s.furu444@ozzio.jp>
2020-04-04 18:49:29 +0900

    finish_jidou

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -62,9 +62,33 @@ $(function(){
     alert("メッセージ送信に失敗しました");
   })
   .always(function(){
-    $('message__text__image').prop('disabled', false);
+    $('.message__text__image').prop('disabled', false);
   })
   })
-
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data(".message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'GET',
+      dataType: 'json',
+      data: { id: last_message_id }
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML = insertHTML + buildHTML(message);
+        })
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error!');
+    });
+    };
+    if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+    }
 
 });
